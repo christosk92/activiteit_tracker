de1092f649e1be74098e186e97b94a067af81cd9
Christos Karapasias <christos@marteco.nl>
2019-12-13 20:05:49 +0100

    test

---
+++ Gyroscope.cs
@@ -0,0 +1,24 @@
+﻿using Iot.Device.CpuTemperature;
+using System;
+
+namespace ActTracker
+{
+    class Gyroscope
+    {
+        static CpuTemperature temp = new CpuTemperature();
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                if (temp.IsAvailable)
+                {
+                    Console.WriteLine($"The cpu temp is {temp.Temperature.Celsius}");
+                }
+                else
+                {
+                    Console.WriteLine("Temp not available");
+                }
+            }
+        }
+    }
+}
